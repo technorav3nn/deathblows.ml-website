d3ebdd373e76ab43ca3439c4be487dbd510f391a
technorav3nn <Colin01460@outlook.com>
2021-06-06 14:20:20 -0400

    Add landing page

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 const { PORT = 3000 } = process.env
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
+    res.sendFile('index.html', {  
+        root: path.join(__dirname, '/public'),
+    });  
+})
+app.get('/landing', (req, res) => {
     res.send('Hello world, from Elastic Beanstalk!')
 })
 
