88e45cdb80bc10b106d53eede327f8f169d8ad32
technorav3nn <Colin01460@outlook.com>
2021-06-06 12:22:50 -0400

    testing added

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const { PORT = 3000 } = process.env
 app.get('/', (req, res) => {
     res.send('Hello world, from Elastic Beanstalk!')
 })
+
+app.get('/test', (req, res) => {
+    const name = req.query.name;
+    if(!name) {
+        res.send("enter a name!!!");
+    }
+
+    res.send(`Your name is ${name}! Hello!`);
+})
+
 app.listen(PORT, () => 
     console.log(`Listening to port ${PORT}`)
 )
