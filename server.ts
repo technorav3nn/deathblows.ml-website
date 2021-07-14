39cd73edb13be82c02bc8bab40298006b444afa8
technorav3nn <colin01460@outlook.com>
2021-07-14 07:21:25 -0700

    new design

---
+++ server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("json spaces", 2);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/views"));
+
+app.get("/", (req, res) => {
+    res.render("index", { test: ["bruh", "hello", "13234", "last one i swear"] });
+});
+
+app.get("/haha", (req: Request, res: Response) => {
+    res.json({
+        response:
+            "haha got you noob you thought something was here? too bad lol stupidass fucker shut up now!!!!!! now brub bur gufdgsdf gjsfd gutrsfdg dfjg ifesgj sfdig jisdfi kgjsdifglongtest fdgndfsg dfhsghl sdfg",
+    });
+});
+
+app.listen(3000, () => console.log("Listening to port 3000!"));
